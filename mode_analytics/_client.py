68001aff616f5cd35b844b76d78bdf057a091c66
Kshitij Aranke <kshitij@aranke.org>
2022-02-27 08:21:18 -0800

    Initial Commit

---
+++ mode_analytics/_client.py
@@ -0,0 +1,77 @@
+import uuid
+from base64 import b64encode
+from datetime import datetime, timedelta, timezone
+from typing import Optional, Literal, Dict, Union
+
+import httpx
+
+from ._models import BatchQueries, Report, ReportRun
+
+
+class ApiClient:
+    def __init__(self,
+                 workspace: str,
+                 token: str,
+                 password: str,
+                 batch: bool = False
+                 ) -> None:
+        self._base_url = f"https://app.mode.com/api/{workspace}"
+        self._auth = httpx.BasicAuth(token, password)
+        self._batch = batch
+
+        if self._batch:
+            self._batch_base_url = f"https://app.mode.com/batch/{workspace}"
+            data = {
+                "signature_token": {
+                    "name": str(uuid.uuid4()),
+                    "expires_at": (datetime.now(timezone.utc) + timedelta(days=1)).isoformat(),
+                    "auth_scope": {"authentication_for": "batch-api", "authorization_type": "read-only"}
+                }
+            }
+
+            r = httpx.post(f"{self._batch_base_url}/signature_tokens", json=data, auth=self._auth)
+            r.raise_for_status()
+
+            rj = r.json()
+
+            bearer_token = b64encode(f"{rj['token']}:{rj['access_key']}:{rj['access_secret']}".encode()).decode()
+            self._header = {"Authorization": "Bearer " + bearer_token}
+
+            self._signature_token_id = rj['token']
+
+    def __enter__(self):
+        return self
+
+    def close(self) -> None:
+        if self._batch:
+            r = httpx.delete(f"{self._batch_base_url}/signature_tokens/{self._signature_token_id}", auth=self._auth)
+            r.raise_for_status()
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        self.close()
+
+    def get_report(self, report: str) -> Report:
+        r = httpx.get(f"{self._base_url}/reports/{report}", auth=self._auth)
+        r.raise_for_status()
+        return Report.parse_obj(r.json())
+
+    def create_report_run(self, report: str, parameters: Dict[str, Union[str, int]]) -> ReportRun:
+        r = httpx.post(f"{self._base_url}/reports/{report}/runs", json=parameters, auth=self._auth)
+        r.raise_for_status()
+        return ReportRun.parse_obj(r.json())
+
+    def list_queries_for_account(self,
+                                 include_spaces: Optional[Literal['all']] = None,
+                                 per_page: int = 1000
+                                 ) -> BatchQueries:
+        assert(self._batch, "Batch Mode must be enabled to call this API")
+
+        params = {'per_page': per_page}
+
+        if include_spaces:
+            params['include_spaces'] = include_spaces
+
+        r = httpx.get(f"{self._batch_base_url}/queries", params=params, headers=self._header)
+        r.raise_for_status()
+
+        return BatchQueries.parse_obj(r.json())
---
+++ mode_analytics/_models.py
@@ -0,0 +1,82 @@
+from __future__ import annotations
+
+from typing import List, Optional, Dict, Literal, Union
+
+from pydantic import BaseModel
+
+
+class Pagination(BaseModel):
+    page: int
+    per_page: int
+    count: int
+    total_pages: int
+    total_count: int
+
+
+class BatchQuery(BaseModel):
+    id: int
+    token: str
+    name: str
+    raw_query: str
+    creator_email: str
+    data_source_token: str
+    report_id: int
+    report_token: str
+    space_id: int
+    space_token: str
+
+
+class BatchQueries(BaseModel):
+    pagination: Pagination
+    queries: List[BatchQuery]
+
+
+class Report(BaseModel):
+    token: str
+    id: int
+    name: str
+    created_at: str
+    updated_at: str
+    edited_at: str
+    theme_id: int
+    color_mappings: str
+    last_successful_sync_at: Optional[str] = None
+    last_saved_at: Optional[str] = None
+    archived: bool
+    account_id: int
+    account_username: str
+    public: bool
+    full_width: bool
+    manual_run_disabled: bool
+    run_privately: bool
+    drilldowns_enabled: bool
+    layout: str
+    is_embedded: bool
+    is_signed: bool
+    shared: bool
+    expected_runtime: float
+    last_successfully_run_at: Optional[str] = None
+    last_run_at: Optional[str] = None
+    web_preview_image: str
+    last_successful_run_token: Optional[str] = None
+    query_count: str
+    chart_count: str
+    schedules_count: str
+    query_preview: str
+    description: str
+    space_token: str
+    flamingo_signature: str
+    github_link: str
+
+
+class ReportRun(BaseModel):
+    token: str
+    state: Literal['pending', 'enqueued', 'cancelled', 'failed', 'succeeded', 'completed', 'running_notebook']
+    parameters: Dict[str, Union[str, int]]
+    created_at: str
+    updated_at: str
+    completed_at: str
+    purge_started_at: str
+    purge_completed_at: str
+    python_state: Literal['none', 'pending', 'failed', 'submitted', 'succeeded']
+    form_fields: List
